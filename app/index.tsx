455a752ae5b6e52e2d25a5529a181b9797897f12
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2026-01-05 23:28:10 +0530

    Feat/Added FCM Notifictaion

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../global.css";
+import { Alert } from "react-native";
 import Mystack from "./navigation/rootnavigation";
-
+import messaging from "@react-native-firebase/messaging";
 const App = () => {
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  };
+  useEffect(() => {
+    requestUserPermission();
+    messaging()
+      .getToken()
+      .then((token) => {
+        console.log("FCM Token:", token);
+      });
+
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            "Notification caused app to open from quit state:",
+            remoteMessage.notification
+          );
+        }
+      });
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      console.log(
+        "Notification caused app to open from background state:",
+        remoteMessage.notification
+      );
+    });
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
   return (
     <>
       <Mystack />
